6905ebe7efcf0fd0ffe8efb0f36bc30c01012e4c
Nando Parga <50nando05@gmail.com>
2025-05-26 12:28:10 +0200

    Exercise29 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,14 @@
 package com.campusdual.classroom;
 
+
+import com.campusdual.util.Utils;
+
 public class Exercise29 {
     public static void main(String[] args) {
-
+       try {
+           throw new Exception("Excepción lanzada intencionadamente");
+       } catch (Exception e) {
+           System.out.println("Ha ocurrido un error inesperado: " + e.getMessage());
+       }
     }
 }
